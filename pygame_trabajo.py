f4c4a50e3348465f95bd4e8e921b31885c5bd5cf
Jamil-Vega-Chachi <jamil.vega.c@uni.pe>
2021-10-04 17:58:18 -0500

    Insertar movimiento derecha

--- pygame_trabajo.py
+++ pygame_trabajo.py
@@ -9,19 +9,27 @@ pantalla = pygame.display.set_mode((600,600))
 
 
 b=pygame.sprite.Sprite()
-b.image = pygame.image.load("E:\mario_3.png").convert_alpha()
+b.image = pygame.image.load("mario_3.png").convert_alpha()
 
-y1=b.image.subsurface((137,355,32,40))
+y1=b.image.subsurface((137,355,32,40)) 
 y2=b.image.subsurface((114,355,32,40))
 y3=b.image.subsurface((91,355,32,40))
 y4=b.image.subsurface((50,355,32,40))
+
+y5=b.image.subsurface((236,355,32,40))
+y6=b.image.subsurface((213,355,32,40))
+y7=b.image.subsurface((190,355,32,40))
+y8=b.image.subsurface((167,355,32,40))
 
 listaMario=[]
 listaMario.append(y1)
 listaMario.append(y2)
 listaMario.append(y3)
 listaMario.append(y4)
-
+listaMario.append(y8)
+listaMario.append(y7)
+listaMario.append(y6)
+listaMario.append(y5)
 
 BLACK = (0,0,0)
 clock = pygame.time.Clock()
@@ -46,15 +54,22 @@ while ejecutando:
     keys=pygame.key.get_pressed()
     
     if keys[pygame.K_LEFT]:
-        temp=1
+        
         i=i+1
-        if(i==4):
+        if(i==8):
             i=0
         x=x-5
+    
+    if keys[pygame.K_RIGHT]:
+       
+        i=i+1
+        if(i==8):
+            i=0
+        x=x+5
         
-    if temp==1:
-        pantalla.fill(BLACK)
-        pantalla.blit(listaMario[i],(x,200))
+   
+    pantalla.fill(BLACK)
+    pantalla.blit(listaMario[i],(x,200))
     
     
     pygame.display.flip()
